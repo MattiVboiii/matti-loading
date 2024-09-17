c43c8f2d70caa8a762a3cb72e47ddeb1794cc023
Matti <57048210+MattiVboiii@users.noreply.github.com>
2024-09-17 23:10:43 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'MattiVboiii'
+description 'Simple & basic loadingscreen script'
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+
+files {
+    'index.html',
+    'music.mp3',
+    'style.css',
+    'background.png',
+    'logo.png'
+}
